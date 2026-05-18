cf689842eaed8b4ad86698eb8c4c50de6ce242c4
riyuexingchen1234 <135087864+riyuexingchen1234@users.noreply.github.com>
2026-05-18 19:25:46 +0800

    Update vite.config.js

--- manju-frontend/vite.config.js
+++ manju-frontend/vite.config.js
@@ -5,19 +5,23 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+export default defineConfig({  
+  plugins: [  
+    vue(),  
+    AutoImport({ resolvers: [ElementPlusResolver()] }),  
+    Components({ resolvers: [ElementPlusResolver()] }),  
+  ],  
+  server: {  
+    proxy: {  
+      '/api': {  
+        target: 'http://localhost:8080',  
+        changeOrigin: true  
+      }  
+    }  
+  },  
+  resolve: {  
+    alias: {  
+      '@': fileURLToPath(new URL('./src', import.meta.url))  
+    }  
+  }  
 })
